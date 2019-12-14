fe5f87991dc5bec1458d58b514a99db6fa76a3ba
cromero180 <54420108+cromero180@users.noreply.github.com>
2019-12-13 22:07:45 -0500

    Added sentence-level extraction

--- surface_terms.py
+++ surface_terms.py
@@ -23,7 +23,7 @@ def remove_trailing_chars(text):
 
 #------------------------#
 
-# Driver script
+# Extract sections
 def extract_sections(corpus = '', search_term = '', start_tag = '', end_tag = '', final_tag = ''):
         
     # Extract start tag up to search term
@@ -64,3 +64,36 @@ def extract_sections(corpus = '', search_term = '', start_tag = '', end_tag = ''
 #     header = header.strip()
     
     return sub, header, end
+
+# Extract sentences
+def extract_sent(corpus = '', 
+                    search_term = '', 
+                    start_sent_tag = '', 
+                    end_sent_tag = '', 
+                    final_sent_tag = '',
+                    final_sent_tag_1 = ''):
+        
+    # Extract start tag up to search term
+    match = re.search(start_sent_tag + '(.*?)' + search_term, corpus, re.DOTALL)
+    sent_start = match.group(0)
+
+    # Get start tag index
+    start = corpus.find(sent_start)
+
+    # Extract search term up to end tag
+    match = re.search(search_term + '(.*?)' + end_sent_tag, corpus, re.DOTALL)
+    sent_end = match.group(0)
+
+    # Get end tag index
+    end = corpus.find(sent_end) + len(sent_end)
+
+    # Isolate sentence
+    sent = corpus[start:end]
+    rev = sent[::-1]
+
+    # Narrow down to last instance of start tag
+    match = re.search(final_sent_tag + '(.*?)' + final_sent_tag_1, rev, re.DOTALL)
+    sent = match.group(0)
+    sent = sent[::-1]
+        
+    return sent, end
